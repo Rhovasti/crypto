04afb10c55b58b6c06fa24584bd4babc0a126b37
Rhovasti <75961332+Rhovasti@users.noreply.github.com>
2021-07-03 10:15:05 +0300

    Update aavegotchi.py

--- aavegotchi.py
+++ aavegotchi.py
@@ -28,12 +28,12 @@ st.write("Find the best value gotchi on the market. \
 Aimed at new frens wanting to get into it without spending a fortune!")
 
 st.write("## Gotchi for sale")
-st.write("Top 20, sorted by rarity per GHST")
-st.table(gotchi_display.sort_values(by=['mbrs/ghst'], ascending=False).head(500))
+st.write("Top 500, sorted by brs")
+st.table(gotchi_display.sort_values(by=['brs'], ascending=False).head(500))
 
 st.write("## Open portals for sale")
 st.write("Top 20, sorted by rarity per GHST")
-st.table(portals_display.sort_values(by=['brs/ghst'], ascending=False).head(500))
+st.table(portals_display.sort_values(by=['brs'], ascending=False).head(500))
 
 
 st.write("Thanks to aavegotchi.land for the API")
