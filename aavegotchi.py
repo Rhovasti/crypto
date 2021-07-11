7ba62233815303e34e54ce2163020e6341a95599
Rhovasti <75961332+Rhovasti@users.noreply.github.com>
2021-07-11 17:16:38 +0300

    Update aavegotchi.py

--- aavegotchi.py
+++ aavegotchi.py
@@ -40,8 +40,8 @@ st.write("Top 500, sorted by rarity per GHST")
 st.table(gotchi_display.sort_values(by=['brs'], ascending=False).head(500))
 
 st.write("## Open portals for sale")
-st.write("Top 500, sorted by rarity per GHST")
-st.table(portals_display.sort_values(by=['min_brs'], ascending=False).head(500))
+st.write("Top 20, sorted by rarity per GHST")
+st.table(portals_display.sort_values(by=['brs/ghst'], ascending=False).head(20))
 
 
 st.write("Thanks to aavegotchi.land for the API")
